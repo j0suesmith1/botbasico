76cb50a7bb41b2bbac9f7728c0da79c58a24c81a
j0suesmith1 <88245151+j0suesmith1@users.noreply.github.com>
2021-08-03 14:26:10 -0500

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,6 +1,6 @@
 const config = {
-        botName: 'ShanBot',
-        ownerName: 'Shan',
+        botName: 'Joseph',
+        ownerName: 'Joseph',
         youtube: 'YOUTUBE_LINK',
         instagram: 'INSTAGRAM_LINK',
 }
