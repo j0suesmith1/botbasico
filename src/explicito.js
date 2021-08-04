dd461418ea94e541fbdcdaec62295482d21c4672
j0suesmith1 <88245151+j0suesmith1@users.noreply.github.com>
2021-08-03 22:42:44 -0500

    Delete explicito.js

--- src/explicito.js
+++
@@ -1,6 +0,0 @@
-const explicito = (prefix, pushname) => {
-    return `const none = fs.readFileSync('./mp3/explicito.mp3');
-		client.sendMessage(from, none, MessageType.audio, {quoted: mek, mimetype: 'audio/mp3', ptt:true})
-                  }
-
-exports.explicito = explicito
