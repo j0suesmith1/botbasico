a7e919a3cca69fe290d24f76716d6bdb22766e7b
j0suesmith1 <88245151+j0suesmith1@users.noreply.github.com>
2021-08-03 19:26:02 -0500

    Update antimenu.js

--- src/antimenu.js
+++ src/antimenu.js
@@ -1,16 +1,58 @@
-const antimenu = (prefix, pushname) => {
+const help2 = (prefix, pushname) => {
     return `
-*Comandos Para Activar El Antilink ⚠*
+⊱✦•𝑴𝑬𝑵𝑼 𝑫𝑬𝑳 𝑩𝑶𝑻⸙۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪۪ࣤ
+ 
+╭┅┅┅ⒸⓄⓂⒶⓃⒹⓄⓈ┅┅┅🌐
+┋
+┋ routersploit
+┋ infoga
+┋ beef
+┋ ngrok
+┋ phoneinfoga
+┋ fotosploit
+┋ sqlmap
+┋ bingen
+┋ socialsploit
+┋ tool-x
+┋ grabscam
+┋ hammer
+┋ kali-nethunter
+┋ cupp
+┋ infectador-framework
+┋ evil-create-framework
+┋ weeman
+┋ fuerza bruta facebook
+┋ wiki-termux
+┋ t-phish
+┋ red_hawk
+┋ doxweb
+┋ recon-spider
+┋ fsociety
+┋ fbi
+┋ seeker
+┋ aiophish
+┋ funlett
+┋ termux-ubuntu
+┋ ip-tracer
+┋ scorpfish v2
+┋ golismero
+┋ angryfuzzer
+┋ recon-dog
+┋ darkfly-tool
+┋ sherlock
+┋ evilurl
+┋ falsify
+┋ syso-termux
+┋ kickthemoutprojet
+┋
+┋ 💎 NOTA:
 
-*PARA ACTIVAR EL ANTILINK*
-${prefix}antilink 1
+┋
+┋ Use los scripts con sabiduria!
 
-*PARA DESACTIVAR EL ANTILINK*
-${prefix}antilink 0
-
-_Para activar este comando el bot necesita tener admin y esta funcion solo la activan los admins del grupo_
-
-ву ѕнαη∂υу
+┋ Queda bajo su propio criterio
+┋ 
+╰┅┅ⒿⓄⓈⒺⓅⒽ ⒷⓄⓉ┅┅🐊
 `
 
 }
